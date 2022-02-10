5d41eff0b25c45f4ef02c60f11f19aa28785ddfb
Camila <yarievakamila@gmail.com>
2022-02-11 02:16:11 +0300

    added OwnerTests

--- src/test/java/yarieva/test/properties/OwnerTests.java
+++ src/test/java/yarieva/test/properties/OwnerTests.java
@@ -1,16 +1,19 @@
 package yarieva.test.properties;
 
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import yarieva.config.CredentialsConfig;
 
 import static java.lang.String.format;
 
+@Tag("properties")
 public class OwnerTests {
 
     public CredentialsConfig credentials =
             ConfigFactory.create(CredentialsConfig.class);
     @Test
+
     void readCredentialsTest() {
         String login = credentials.login();
         String password = credentials.password();
